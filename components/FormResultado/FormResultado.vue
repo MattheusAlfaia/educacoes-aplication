2e4a46773d486530561df2ff35fa9170ac817f1e
MattheusAlfaia <83054761+MattheusAlfaia@users.noreply.github.com>
2023-10-02 15:58:36 -0400

    Form Fix

--- components/FormResultado/FormResultado.vue
+++ components/FormResultado/FormResultado.vue
@@ -1,12 +1,12 @@
 <template>
     <section>
-        <form @submit.prevent="pesquisaSubmit" class="mt-1">
+        <form @submit.prevent="graduacaoSubmit" class="mt-1">
             <div class="row col">
                 <div class="col-lg-4">
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1"><i class="bi bi-easel"></i></span>
-                        <!-- @change="filtrodataBusca(dataBusca)" v-model="dataBusca" -->
-                        <select class="form-select" aria-label="Nível de Ensino">
+                        <!-- @change="filtrodataBusca(dataBusca)" v-model="nomeNivel" -->
+                        <select class="form-select" aria-label="Nível de Ensino" v-model="nomeNivel">
                             <option value="GRADUAÇÃO" selected>{{ dataBusca.nomeNivel }}</option>
                             <!-- <option v-for="(dataBusca, index) in dataBuscas" :key="index" :value="dataBusca.nome">
                             {{ dataBusca.nome }}
@@ -18,30 +18,71 @@
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1"><i class="bi bi-geo-alt"></i></span>
                         <input type="text" spellcheck="false" class="form-control curso"
-                            placeholder="Cidade de sua preferência" aria-label="Cidade" :value="dataBusca.nomeCidade"
-                            aria-describedby="basic-addon1" name="curso" />
+                            placeholder="Cidade de sua preferência" aria-label="Cidade" v-model="nomeCidade"
+                            aria-describedby="basic-addon1" name="nomeCidade" />
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="input-group mb-3">
+                        <!-- :value="dataBusca.nomeCurso" -->
                         <input type="text" class="form-control curso" placeholder="Curso" aria-label="Curso"
-                            :value="dataBusca.nomeCurso" aria-describedby="basic-addon1" name="curso" />
-                        <button class="btn btn-outline-secondary" type="submit"><i class="bi bi-search"></i></button>
+                             aria-describedby="basic-addon1" name="curso" v-model="nomeCurso" />
+                        <button type="submit" class="btn btn-outline-secondary"><i class="bi bi-search"></i></button>
                     </div>
                 </div>
             </div>
+            <!-- {{ dataBusca }} -->
         </form>
     </section>
 </template>
 
 <script>
-// import { cursoStore, pinia } from '@/stores/getCursos';
+import { cursoStore, pinia } from '@/stores/getCursos';
 
 export default defineComponent({
     name: 'FormResultado',
     props: {
         dataBusca: Object,
     },
+    setup (){
+        const nomeNivel = ref (null);
+        const nomeModalidade = ref(null);
+        const nomeCidade = ref(null);
+        const nomeCurso = ref(null);
+        const nomeArea = ref(null);
+        const nomeEstado = ref(null);
+        const nomeTurno = ref(null);
+        const anoEscolar = ref(null);
+
+        const graduacaoSubmit = () => {
+            const data = {
+                nomeNivel: nomeNivel.value,
+                nomeModalidade: '',
+                nomeCurso: nomeCurso.value,
+                nomeArea: '',
+                nomeTurno: '',
+                nomeCidade: nomeCidade.value,
+                nomeEstado: '',
+            };
+            const myStore = cursoStore(pinia);
+            myStore.setCursos(data);
+            localStorage.setItem('data', JSON.stringify(data));
+            window.location.reload();
+        };
+
+        return{
+            nomeNivel,
+            nomeModalidade,
+            nomeCidade,
+            nomeCurso,
+            nomeArea,
+            nomeEstado,
+            nomeTurno,
+            anoEscolar,
+            graduacaoSubmit,
+        }
+    }
+
 });
 </script>
 
