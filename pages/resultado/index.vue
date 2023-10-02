0fce1c1a0957e6f684b1de03cd17e976b1aa95f2
MattheusAlfaia <83054761+MattheusAlfaia@users.noreply.github.com>
2023-10-02 16:13:22 -0400

    Style FormResult

--- pages/resultado/index.vue
+++ pages/resultado/index.vue
@@ -32,7 +32,7 @@
             </div>
             <!-- ==================== -->
             <!-- colocar n acordion -->
-            <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion accordion-flush" id="formResultado">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="flush-headingOne">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -41,7 +41,7 @@
                         </button>
                     </h2>
                     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
-                        data-bs-parent="#accordionFlushExample">
+                        data-bs-parent="#formResultado">
                         <div class="accordion-body">
                             <FormResultado :dataBusca="dataBusca" />
                         </div>
