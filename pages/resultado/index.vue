085fcce02aff392c61c251333b3c613106f10415
MattheusAlfaia <83054761+MattheusAlfaia@users.noreply.github.com>
2023-10-03 09:32:51 -0400

    Up Styles

--- pages/resultado/index.vue
+++ pages/resultado/index.vue
@@ -5,6 +5,24 @@
     </div>
     <div v-else>
         <section class="mt-3 container">
+            <!-- ==================== -->
+            <div class="accordion accordion-flush" id="formResultado">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            <span class="accordion-title">Alterar sua busca</span>
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+                        data-bs-parent="#formResultado">
+                        <div class="accordion-body">
+                            <FormResultado :dataBusca="dataBusca" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ================= -->
             <div class="row mb-2">
                 <div class="d-flex justify-content-between">
                     <div class="col-sm-4 col-md-6 text-start">
@@ -30,26 +48,6 @@
                     </div>
                 </div>
             </div>
-            <!-- ==================== -->
-            <!-- colocar n acordion -->
-            <div class="accordion accordion-flush" id="formResultado">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingOne">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            <span class="accordion-title">Alterar sua busca</span>
-                        </button>
-                    </h2>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
-                        data-bs-parent="#formResultado">
-                        <div class="accordion-body">
-                            <FormResultado :dataBusca="dataBusca" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- ================= -->
             <section class="container">
                 <div class="row">
                     <!-- {{ resposta.ies }} -->
