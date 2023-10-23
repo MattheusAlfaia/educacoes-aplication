ca5701f0c26768fd400a9765dd35c453eeecd7e3
MattheusAlfaia <83054761+MattheusAlfaia@users.noreply.github.com>
2023-10-23 15:09:27 -0400

    New Features

--- app.vue
+++ app.vue
@@ -5,13 +5,20 @@
     <FooterPage />
 </template>
 
-<script>
+<script setup lang="ts">
+useSeoMeta({
+    title: 'INSTITUTO EDUCAÇÕES - Bolsas de Estudo',
+    ogTitle: 'INTERATIVO ATIVIDADES DE APOIO A EDUCAÇÃO LTDA',
+    description: 'Bolsas de estudo para educação básica, graduação, pós-graduação, cursos Técnicos, cursos livres, supletivo e idiomas.',
+    ogDescription: 'This is my amazing site, let me tell you all about it.',
+    ogUrl: 'https://www.educacoes.com.br/',
+    ogType: 'website',
+    ogSiteName: 'Instituto Educações - Bolsas de Estudos',
+    ogLocale: 'pt_BR',
+    authorName: 'Mattheus Alfaia, alfaiamattheus@gmail.com',
+    robots: 'index, follow',   
+})
+
 import NavBar from '~/components/NavBar/NavBar.vue'
 import FooterPage from './components/Footer/FooterPage.vue';
-export default {
-    components: {
-        NavBar,
-        FooterPage
-    }
-}
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', 'nuxt-bootstrap-icons', 'vue3-carousel-nuxt'],
+  modules: ['@pinia/nuxt', "nuxt-security", 'nuxt-bootstrap-icons', 'vue3-carousel-nuxt'],
   buildModules: ['@nuxt/axios'],
   css: ['bootstrap/dist/css/bootstrap.min.css', 'bootstrap-icons/font/bootstrap-icons.min.css'],
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  title: 'Instituto Educações',
+  
 })
-
-//  modules: ['@vite-pwa/nuxt', '@pinia/nuxt', '@nuxt/ui', 'vue3-carousel-nuxt','nuxt-bootstrap-icons'],
-
-// yarn add nuxt-bootstrap-icons
-// yarn add bootstrap
-// yarn add --dev sass sass-loader@latest
